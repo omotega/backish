5c67261664a08b78a38c5b1d77d83357a81ca02b
omotega <tomoyibo@gmail.com>
2023-10-11 18:26:52 +0100

    added get-all-users-in-organization endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router, Request, Response, NextFunction } from "express";
 import { errorHandler } from "../midlewares/errors/errorhandler";
 import userRouter from "./userroute";
 import fileRouter from "./fileroute";
+import orgRouter from "./orgroute";
 
 const route = Router();
 
 route.use("/user", userRouter);
 route.use("/file", fileRouter);
+route.use("/org", orgRouter);
 
 route.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log("Error encountered:", err.message || err);
