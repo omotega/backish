cde5445962e21d52b5f38ef63121ef4d1b2efe9d
omotega <tomoyibo@gmail.com>
2023-10-14 08:45:42 +0100

    feat:added signout organization endpoint

--- src/services/organizatioservices.ts
+++ src/services/organizatioservices.ts
@@ -3,6 +3,7 @@ import usermodel from "../database/model/usermodel";
 import { AppError } from "../utils/errors";
 import httpStatus from "http-status";
 import messages from "../utils/messages";
+import helperServices from "./helper-services";
 
 
 const listAllUsersInOrganization = async (orgId: string) => {
@@ -49,7 +50,51 @@ const findUser = async ({ orgId, email }: { orgId: string; email: string }) => {
     });
   return userDetails;
 };
+
+const leaveOrganization = async ({
+  orgId,
+  userId,
+}: {
+  orgId: string;
+  userId: string;
+}) => {
+  const isUser = await helperServices.getUserdetailsById(userId);
+  const result: any = isUser.orgStatus.find(
+    (item) => item.orgId?.toString() === orgId.toString()
+  );
+  if (!result) return;
+  const index = isUser.orgStatus.indexOf(result);
+  console.log(index, "this is the index");
+  if (index !== -1) {
+    isUser.orgStatus.splice(index, 1);
+  }
+  isUser.save();
+
+  const userExistinOrg = await Organization.findOne({
+    _id: orgId,
+    invitedEmails: { $in: [isUser.email] },
+  }).select("invitedEmails");
+  if (!userExistinOrg)
+    throw new AppError({
+      httpCode: httpStatus.NOT_FOUND,
+      description: messages.USER_NOT_FOUND,
+    });
+  if (!userExistinOrg.invitedEmails) return;
+  const results: any = userExistinOrg.invitedEmails.find(
+    (item) => item === isUser.email
+  );
+  if (!results) return;
+  const emailIndex = userExistinOrg.invitedEmails.indexOf(results);
+  if (emailIndex !== -1) {
+    userExistinOrg.invitedEmails.splice(emailIndex, 1);
+  }
+  userExistinOrg.save();
+  const message = `you have left ${userExistinOrg.orgName} organization`;
+
+  return "success";
+};
 export default {
   listAllUsersInOrganization,
   findUser,
+  leaveOrganization,
 }
